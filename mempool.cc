b427a018d74ec66ab76d6a393057beab1883b495
Ivan Ivanov <Ivan.Ivanov@kordia.co.nz>
2017-07-09 06:28:31 +1200

    Add chunks to pool

--- mempool.cc
+++ mempool.cc
@@ -1,19 +1,46 @@
+#include <stdlib.h>
+#include <assert.h>
+
 #include "mempool.h"
+
+// TODO: add function comments
 
 Mempool::Mempool(unsigned int element_size, unsigned int per_chunk,
                  unsigned int alloc_total) {
-  element_size_ = element_size;
+  // Calculate sizes of chunk and element including the headers
   per_chunk_ = per_chunk;
+  element_size_ = sizeof(Mempool_Element) + element_size;
+  chunk_size_ = sizeof(Mempool_Chunk) + element_size_ * per_chunk;
   total_used_ = 0;
 
-  // How many chunks do we need now
-  int chunks_to_allocate = 1;
+  // How many chunks do we need to allocate now
+  int chunks = 1;
   if (alloc_total > per_chunk) {
-    chunks_to_allocate = (alloc_total / per_chunk) + 1;
+    chunks = (alloc_total / per_chunk) + 1;
   }
 
+  // Allocate actual chunks and add them to the pool
+  Mempool_Element *last_element = nullptr;
+  for (int i = 0; i < chunks; ++i) {
+    Mempool_Chunk *chunk = (Mempool_Chunk *)malloc(chunk_size_);
+    last_element = AddChunk(chunk, last_element);
+  }
+}
 
+Mempool_Element *Mempool::AddChunk(Mempool_Chunk *chunk,
+                                   Mempool_Element *last_element) {
+  Mempool_Element *tail = nullptr;
+  // TODO:
 
+  return tail;
+}
+
+/**
+ * Returns a pointer to the area of memory immediately following
+ * the chunk head (and that is where the first element of the chunk is)
+ */
+Mempool_Element *Mempool::FirstElement(Mempool_Chunk *chunk) {
+  return (Mempool_Element *)(chunk + 1);
 }
 
 int main() {}
--- mempool.h
+++ mempool.h
@@ -14,16 +14,22 @@ class Mempool {
   Mempool(unsigned int element_size, unsigned int per_chunk,
           unsigned int alloc_total);
 
-  Mempool_Element *allocate_element();
-  void free_element(Mempool_Element *element);
+  Mempool_Element *AllocateElement();
+  void FreeElement(Mempool_Element *element);
 
  private:
-  unsigned int element_size_;  // size of element, in bytes
-  unsigned int per_chunk_;     // number of elements per chunk
-  unsigned int total_used_;    // number of elements in use
+  unsigned int chunk_size_;         // size of chunk, in bytes
+  unsigned int element_size_;       // size of element, in bytes
+  unsigned int per_chunk_;          // number of elements per chunk
+  unsigned int total_used_ = 0;     // number of elements in use
 
-  Mempool_Chunk *chunks_;
-  Mempool_Chunk *last_chunk_;
+  Mempool_Chunk *chunks_ = nullptr;
+  Mempool_Chunk *last_chunk_ = nullptr;
+  Mempool_Element *free_element = nullptr;
+
+  Mempool_Element *AddChunk(Mempool_Chunk *chunk,
+                            Mempool_Element *last_element);
+  Mempool_Element *FirstElement(Mempool_Chunk *chunk);
 };
 
 #endif  // _MEMPOOL_H_
