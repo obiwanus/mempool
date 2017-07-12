7d9ded0ba48583dcf6c10cac605f35b2979fcc9d
Ivan Ivanov <Ivan.Ivanov@kordia.co.nz>
2017-07-13 05:56:34 +1200

    Very basic benchmarks

--- benchmark.cc
+++ benchmark.cc
@@ -4,6 +4,10 @@
 #define MEMPOOL_DEBUG
 #include "mempool.cc"
 
+double time_between(clock_t begin, clock_t end) {
+  return (double)(end - begin) / CLOCKS_PER_SEC;
+}
+
 int main() {
   Mempool pool(64, 256, 100);
   clock_t begin, end;
@@ -11,24 +15,37 @@ int main() {
 
   const int kObjectCount = 50000000;
 
+  // Test C++ new/delete allocation
+  Mempool_Element **elements = new Mempool_Element *[kObjectCount];
+  begin = clock();
+  for (int i = 0; i < kObjectCount; ++i) {
+    elements[i] = new Mempool_Element();
+  }
+  end = clock();
+  time_spent = time_between(begin, end);
+  printf("C++ new: allocated in %.2f seconds\n", time_spent);
+
   // Test pool
   begin = clock();
   for (int i = 0; i < kObjectCount; ++i) {
     Mempool_Element *el = pool.AllocateElement();
   }
   end = clock();
-  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent = time_between(begin, end);
   printf("Pool: allocated in %.2f seconds...\n", time_spent);
+
+  begin = clock();
+  for (int i = 0; i < kObjectCount; ++i) {
+    delete elements[i];
+  }
+  end = clock();
+  time_spent = time_between(begin, end);
+  printf("C++ delete: freed in %.2f seconds\n", time_spent);
 
   begin = clock();
   pool.Destroy();
   end = clock();
-  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  time_spent = time_between(begin, end);
   printf("Pool: freed in %.2f seconds\n", time_spent);
 
-  // Test individual allocation
-  Mempool_Element *elements = (Mempool_Element *)malloc(kObjectCount * sizeof(Mempool_Element *));
-  for (int i = 0; i < kObjectCount; ++i) {
-
-  }
 }
